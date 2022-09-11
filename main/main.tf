d795e1493a138fd415e2fb660f9995dabbbe2b39
greatjack1 <yaakovrim@gmail.com>
2022-09-11 14:35:02 -0400

    add terraform required version and provider

--- main/main.tf
+++ main/main.tf
@@ -1,4 +1,15 @@
 
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+  required_version = "1.2"
+}
+
+
 module "Auth" {
     source = "Auth"
 }
