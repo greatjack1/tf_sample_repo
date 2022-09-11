83e7bbb41a98c33ff5172e9f15c94b42b1586cd2
greatjack1 <yaakovrim@gmail.com>
2022-09-11 16:12:09 -0400

    Add valid arn

--- modules/Compute/lambda.tf
+++ modules/Compute/lambda.tf
@@ -1,4 +1,25 @@
+resource "aws_iam_role" "iam_for_lambda_tf" {
+  name = "iam_for_lambda_tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "function" {
 	function_name = "test_function"
+ 	role  = aws_iam_role.iam_for_lambda_tf.arn
 	runtime = "python3.9"
 }
